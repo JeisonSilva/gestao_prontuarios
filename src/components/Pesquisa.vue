ef9ca6dc813e8ce6e4ec386256cd9720b83991f4
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2019-10-08 07:56:06 -0300

    criado cadastro e pesquisa de prontuarios

--- src/components/Pesquisa.vue
+++ src/components/Pesquisa.vue
@@ -1,51 +1,123 @@
 <template>
   <div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <div class="form-group">
-        <input v-model="pesquisa" type="text" class="form-control" placeholder="pesquise por um nome">
-        <br>
-        <button class="btn btn-success">PESQUISAR</button>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <div class="row">
+        <div class="col-sm-12 col-md-10 col-sx-10">
+            <div class="form-group">
+                <input v-model="pesquisa" type="text" class="form-control" placeholder="digite sua pesquisa (RE ou NOME)">
+                
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-2 col sx-2">
+            <button class="btn btn-success" v-on:click="pesquisar(pesquisa)">PESQUISAR</button>
+        </div>
     </div>
 
-    <div class="list-group">
-        <div v-for="prontuario in prontuarios" v-bind:key="prontuario.re" class="list-group-item list-group-item-action">
-            <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">{{prontuario.nome}}</h5>
-            <small>
-                <button class="btn btn-primary" v-on:click="favoritar(prontuario.re)">
-                    <i  v-if="prontuario.favoritado" class="fa fa-star"></i>
-                    <i  v-else class="fa fa-star-o"></i>
-                </button>
-            </small>
+    <div class="row">
+        <div class="col-sm-12 col-md-10 col-sx-10">
+            <div class="list-group">
+                <div v-for="prontuario in prontuarios" v-bind:key="prontuario.id" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">{{prontuario.nome}}</h5>
+                <small>
+                    <button class="btn btn-primary" v-on:click="favoritar(prontuario.re, pesquisa)">
+                        <i  v-if="prontuario.favoritado" class="fa fa-star"></i>
+                        <i  v-else class="fa fa-star-o"></i>
+                    </button>
+                </small>
             </div>
             <small>Re. {{prontuario.re}}</small>
         </div>
     </div>
+        </div>
+    </div>  
+    
+
+    
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Vue from 'vue';
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/index.css';
+
+Vue.use(VueToast,{
+  position: 'top'
+});
+
 export default {
     data: function () {
         return {
             pesquisa: '',
-            prontuarios: [
-                {
-                    re: 1,
-                    nome: 'Jeison O. da Silva',
-                    favoritado: false
-                }
-            ]
+            prontuarios:[],
+            url_base:'http://localhost:3000'
         }
     },
     methods:{
-        favoritar:function (re) {
-            let prontuario = this.prontuarios.find(x=>x.re === re)
-            if(prontuario != null || prontuario != undefined){
-                prontuario.favoritado = !prontuario.favoritado 
-                this.prontuarios.pop(x=>x.re === re)
-                this.prontuarios.push(prontuario)
-            }
+        favoritar:function (re, textoPesquisa) {              
+            this.prontuarios.forEach(element =>{
+                if(element.re === re){
+                    element.favoritado = !element.favoritado
+
+                    if(element.favoritado){
+                        this.ativarFavorito(element)
+                        this.registrarFavorito(element)
+                    }else{                        
+                        this.desativarFavorito(element)
+                        this.removerFavorito(element)
+                    }
+                }
+            })
+        },
+        pesquisar: function (nome) {
+            let vm = this
+
+            axios.get(`${this.url_base}/prontuarios?q=${nome}`)
+                .then(function (response){
+                    vm.prontuarios = response.data
+                })            
+        },
+        ativarFavorito: function (prontuario) {
+            axios.patch(`${this.url_base}/prontuarios/${prontuario.re}`,{
+                    favoritado: true
+                })
+                .then(function (response) {
+                    this.pesquisar(textoPesquisa)
+                })
+                .catch(function (error) {
+                })
+        },
+        desativarFavorito: function (prontuario) {
+            axios.patch(`${this.url_base}/prontuarios/${prontuario.re}`,{
+                    favoritado: false
+                })
+                .then(function (response) {
+                    this.pesquisar(textoPesquisa)
+                })
+                .catch(function (error) {
+                })
+        },
+        registrarFavorito: function (prontuario) {
+            axios.post(`${this.url_base}/favoritos`, prontuario)
+                .then(function (reponse) {
+                    Vue.$toast.success('Prontuário favoritado')
+                })
+                .catch(function (error) {
+                    Vue.$toast.error('Falha ao tentar favoritar')
+                })
+        },
+        removerFavorito: function (prontuario) {
+            axios.delete(`${this.url_base}/favoritos/${prontuario.id}`)
+            .then(function (reponse) {
+                    Vue.$toast.success('Prontuário excluído dos favoritos')
+                })
+                .catch(function (error) {
+                    Vue.$toast.error('Falha ao tentar excluir dos favoritos')
+                })
         }
     }    
 }
--- src/components/Registro.vue
+++ src/components/Registro.vue
@@ -1,15 +1,88 @@
 <template>
-  <div>
-      <p>Ola Registro</p>
+  <div>    
+      <div class="card">
+        <div class="card-body">
+          <div class="card-title">Registro de Prontuários</div>
+
+          <div class="form-group">
+            <input v-model="prontuario.re" type="number" id="txt-re" class="form-control" placeholder="re">
+          </div>
+          <div class="form-group">
+            <input v-model="prontuario.nome" type="text" id="txt-nome" class="form-control" placeholder="nome">
+          </div>
+
+          <button v-on:click="registrar()" class="btn btn-success">REGISTRAR</button>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import Vue from 'vue';
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/index.css';
 
+Vue.use(VueToast,{
+  position: 'top'
+});
+
+export default {
+  data:function () {
+    return {
+      prontuario:{
+        re: 0,
+        nome: ''
+      },
+      url_base:'http://localhost:3000/prontuarios/'
+    }
+  },
+  methods:{
+    registrar:function () {
+      var vm = this
+
+      if(this.validarDadosProntuario(this.prontuario)){
+        axios.post(this.url_base, {
+          re: this.prontuario.re, 
+          nome: this.prontuario.nome.toUpperCase(),
+          favoritado: false})
+          .then(function (response) {
+            Vue.$toast.success(`${vm.prontuario.nome} foi registrado com sucesso`)
+            vm.prontuario.nome =''
+            vm.prontuario.re = 0
+          })
+          .catch (function (err) {
+            Vue.$toast.error(`Não foi possível registrar ${vm.nome}`)
+          })
+      }
+
+      
+    },
+    validarDadosProntuario:function (prontuario){
+      if(prontuario.re === 0){        
+        Vue.$toast.error("Preencher o RE")
+        return false
+      }
+
+      if(prontuario.nome === ''){        
+        Vue.$toast.error("Preencher o Nome")
+        return false
+      }
+
+      return true
+    }
+  }
 }
 </script>
 
 <style>
+.card{
+  width: 50%;
+  float: left;
+  margin: 5% 0% 0% 25%
+}
 
+#txt-re{
+  width: 20%
+}
 </style>
